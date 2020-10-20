6b42eb57ba7b9ddbba5cbf99e5a6bc4148bbb676
jaal99 <57971905+jaal99@users.noreply.github.com>
2020-10-20 11:44:57 -0600

    Habilitamos handelabrs como template engine y body-parser para obtener el cuerpo de la petición

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const express = require("express");
 require("./config/db");
 const exphbs = require("express-handlebars");
 const router = require("./routes/index");
+const bodyParser = require("body-parser");
 
 //Habilitar el archivo de variables de entorno
 require("dotenv").config({ path: ".env" });
 
 // Crear un servidor utilizando express
 const app = express();
+
+// Habilitar Handlebars como nuestro template engine
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+
+app.set("view engine", "hbs");
+
+// Habilitar body-parser para obtener el cuerpo de la petición
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Implementar router
 app.use("/", router());
