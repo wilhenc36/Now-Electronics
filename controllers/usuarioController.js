3a3bb49b1e12bec81d29543f3bc8c66197226ed0
jaal99 <57971905+jaal99@users.noreply.github.com>
2020-10-21 21:10:55 -0600

    Try catch para recibir los datos de registro en la base en mongo db

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -8,6 +8,23 @@ exports.formularioCrearCuenta = (req, res, next) => {
 };
 
 // Procesar el formulario de creación de cuenta
-exports.crearCuenta = (req, res, next) => {
-    console.log(req.body);
+exports.crearCuenta = async (req, res, next) => {
+  // Obtener las variables desde el cuerpo de la petición
+  const { nombre, email, password } = req.body;
+
+  // Intentar almacenar los datos del usuario
+  try {
+    // Crear el usuario
+    // https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Promise
+    // https://developer.mozilla.org/es/docs/Learn/JavaScript/Asynchronous/Async_await
+    await Usuario.create({
+      email,
+      password,
+      nombre,
+    });
+
+    // Mostrar un mensaje
+  } catch (error) {
+    console.log(error);
+  }
 };
