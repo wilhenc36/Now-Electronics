94d0e42e515b52e2092811baa7aea76ccbfa0997
jaal99 <57971905+jaal99@users.noreply.github.com>
2020-10-23 04:34:32 -0600

    Creación del auth controller.

---
+++ controllers/authController.js
@@ -0,0 +1,24 @@
+// Importar los módulos requeridos
+const passport = require("passport");
+const mongoose = require("mongoose");
+const Usuario = mongoose.model("Usuarios");
+
+// Se encarga de autenticar el usuario y de redireccionarlo
+exports.autenticarUsuario = passport.authenticate("local", {
+  successRedirect: "/administrar",
+  failureRedirect: "/iniciar-sesion",
+  failureFlash: true,
+  badRequestMessage: ["Debes ingresar tus credenciales"],
+});
+/*
+// Cerrar la sesión del usuario
+exports.cerrarSesion = (req, res, next) => {
+  // Cierra la sesión
+  req.logout();
+
+  req.flash("success", [
+    "Has cerrado correctamente tu sesión. ¡Vuelve pronto!",
+  ]);
+
+  return res.redirect("/iniciar-sesion");
+};*/
