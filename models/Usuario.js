b0008a9f4512013cf27b03fa984caeb1fe7d5828
jaal99 <57971905+jaal99@users.noreply.github.com>
2020-10-23 01:15:22 -0600

    Implementamos Hooks para password y para almacenar fecha de registro del usuario.

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,5 +1,6 @@
 // Importar los módulos requeridos
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 // Definición del schema
 const usuarioSchema = new mongoose.Schema({
@@ -26,6 +27,68 @@ const usuarioSchema = new mongoose.Schema({
     expira: Date,
     gravatar: String,
     activo: Boolean,
+    fechaRegistro: Date,
   });
+
+// https://mongoosejs.com/docs/middleware.html#order
+// Hooks hash del password (hash + salt)
+usuarioSchema.pre("save", function (next) {
+  const user = this;
+
+  // Si el password fué modificado
+  if (!user.isModified("password")) {
+    return next();
+  }
+// Generar el salt y si no existe error en la generación
+  // realizar el hash del password. Se almacena el hash + salt
+  // para evitar ataques rainbow table.
+  bcrypt.genSalt(10, (err, salt) => {
+    // Si existe un error continuar
+    if (err) return next(err);
+
+    // Si se generó el salt, realizar el hash
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      // Si existe un error continuar
+      if (err) return next(err);
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// Hooks activar el usuario y almacenar la fecha de registro
+usuarioSchema.pre("save", function (err, doc, next) {
+  // TODO: Revisar la inserción para el hook
+  const user = this;
+
+  if (err) return next(err);
+  else {
+    user.activo = true;
+    user.fechaRegistro = Date.now();
+    user.save();
+  }
+
+  next();
+});
+
+// Realizar un método que automáticamente verifique si el password candidato
+// ingresado por el usuario es igual al almacenado en la BD (hash + salt)
+usuarioSchema.methods.comparePassword = function (candidatePassword) {
+  const user = this;
+
+  return new Promise((resolve, reject) => {
+    // Comparar el password candidato con el password almacenado
+    bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+      // Promesa incumplida
+      if (err) return reject(err);
+
+      // Promesa cumplida
+      if (!isMatch) return reject(err);
+
+      resolve(true);
+    });
+  }).catch(console.log("Error al momento de comparar los passwords"));
+};
 
   module.exports = mongoose.model("Usuarios", usuarioSchema);
