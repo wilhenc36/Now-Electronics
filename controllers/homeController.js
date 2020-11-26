eafd2922cc2eb1d14a453e972d090fe228e3fe87
hertder <57971607+hertder@users.noreply.github.com>
2020-11-26 17:50:11 -0500

    Obtener los productos disponibles

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,10 +2,8 @@ const mongoose = require("mongoose");
 const Producto = mongoose.model("Producto");
 
 exports.mostrarProductos = async (req, res, next) => {
-    //Obtener todos los productos disponibles
-    const productos = await Producto.find()
+  // Obtener todos los productos disponibles
+  const productos = await Producto.find().lean();
 
-    console.log(productos);
-
-    res.render("buscar", { productos });
+  res.render("buscar", { productos });
 };
