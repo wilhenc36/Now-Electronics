79031c64edfb2efba30debc497a705eaa71d6b78
hertder <57971607+hertder@users.noreply.github.com>
2020-11-26 17:53:11 -0500

    Implementacion y visualizacion de los productos atravez de url y aunento del tamano en la base de datos por producto

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -1,6 +1,7 @@
 // Importar los módulos requeridos
 const mongoose = require("mongoose");
 const Producto = mongoose.model("Producto");
+const Carrito = mongoose.model("Carrito");
 const { validationResult } = require("express-validator");
 const multer = require("multer");
 const shortid = require("shortid");
@@ -60,65 +61,64 @@ exports.crearProducto = async (req, res, next) => {
       res.redirect("/crear-producto");
     }
   }
-
-  // res.redirect("/crear-producto");
 };
 
 // Permite subir un archivo (imagen) al servidor
 exports.subirImagen = (req, res, next) => {
   // Verificar que no existen errores de validación
   const errores = validationResult(req);
+  //const errores = [];
   const messages = [];
 
-  if (!errores.isEmpty) {
-    errores.array().map((error) => {
-      messages.push({ message: error.msg, alertType: "danger" });
-    });
+   if (!errores.isEmpty) {
+     errores.array().map((error) => {
+       messages.push({ message: error.msg, alertType: "danger" });
+     });
 
-    req.flash("messages", messages);
-    res.redirect("/crear-producto");
-  } else {
-    // Subir el archivo mediante Multer
-    upload(req, res, function (error) {
-      console.log(req.body);
-      if (error) {
-        // Errores de Multer
-        if (error instanceof multer.MulterError) {
-          if (error.code === "LIMIT_FILE_SIZE") {
-            req.flash("messages", [
-              {
-                message:
-                  "El tamaño del archivo es superior al límite. Máximo 300Kb",
-                alertType: "danger",
-              },
-            ]);
-          } else {
-            req.flash("messages", [
-              { message: error.message, alertType: "danger" },
-            ]);
-          }
+     req.flash("messages", messages);
+     res.redirect("/crear-producto");
+   } else {
+  // Subir el archivo mediante Multer
+  upload(req, res, function (error) {
+    console.log(req.body);
+    if (error) {
+      // Errores de Multer
+      if (error instanceof multer.MulterError) {
+        if (error.code === "LIMIT_FILE_SIZE") {
+          req.flash("messages", [
+            {
+              message:
+                "El tamaño del archivo es superior al límite. Máximo 300Kb",
+              alertType: "danger",
+            },
+          ]);
         } else {
-          // Errores creado por el usuario
           req.flash("messages", [
             { message: error.message, alertType: "danger" },
           ]);
         }
-        // Redireccionar y mostrar el error
-        res.redirect("/crear-producto");
-        return;
       } else {
-        // Archivo cargado correctamente
-        return next();
+        // Errores creado por el usuario
+        req.flash("messages", [
+          { message: error.message, alertType: "danger" },
+        ]);
       }
-    });
-  }
+      // Redireccionar y mostrar el error
+      res.redirect("/crear-producto");
+      return;
+    } else {
+      // Archivo cargado correctamente
+      return next();
+    }
+  });
+   }
 };
 
 // Opciones de configuración para multer en productos
 const configuracionMulter = {
   // Tamaño máximo del archivo en bytes
   limits: {
-    fileSize: 300000,
+    fileSize: 1000000,
   },
   // Dónde se almacena el archivo
   storage: (fileStorage = multer.diskStorage({
@@ -150,5 +150,86 @@ const configuracionMulter = {
   },
 };
 
+// Muestra un producto que se obtiene a través de su URL
+exports.verProducto = async (req, res, next) => {
+  // Utilizar la opción populate para obtener información sobre un Object_ID
+  const producto = await Producto.findOne({ url: req.params.url })
+    .populate("vendedor")
+    .lean();
+
+  // Buscar productos en el carrito de compras si existen
+  const carrito = await Carrito.findOne({ usuario: req.user._id });
+
+  if (!producto) res.redirect("/");
+  else {
+    res.render("mostrarProducto", {
+      producto,
+      productosCarrito: carrito ? carrito.producto.length : 0,
+    });
+  }
+};
+
 // Función que sube el archivo
 const upload = multer(configuracionMulter).single("imagen");
+
+// Agrega productos al carrito de compras
+exports.agregarProductoCarrito = async (req, res, next) => {
+  try {
+    // Obtener el producto a través del URL
+    const { url } = req.params;
+
+    const producto = await Producto.findOne({ url });
+
+    // Buscar si el usuario ya tiene un carrito existente
+    const carrito = await Carrito.findOne({ usuario: req.user._id });
+
+    console.log(carrito);
+
+    // Si el carrito no existe
+    if (!carrito) {
+      // Crear el arreglo de productos
+      const productos = [];
+      productos.push(producto);
+
+      const nuevoCarrito = new Carrito({
+        producto: productos,
+        usuario: req.user._id,
+        fecha: Date.now(),
+        total: producto.precio,
+      });
+
+      // Almacenar el carrito
+      await nuevoCarrito.save();
+
+      req.flash("messages", [
+        {
+          message: "Producto agregado a tu carrito de compras",
+          alertType: "success",
+        },
+      ]);
+
+      // Redireccionar
+      res.redirect("/");
+    }
+
+    // Ya existe un carrito almacenado para el usuario
+    carrito.producto.push(producto);
+
+    // Actualizar el total del carrito
+    carrito.total += producto.precio;
+
+    // Almacenar el nuevo producto
+    await carrito.save();
+
+    req.flash("messages", [
+      {
+        message: "Producto agregado a tu carrito de compras",
+        alertType: "success",
+      },
+    ]);
+
+    res.redirect("/");
+  } catch (error) {
+    // console.log(error);
+  }
+};
