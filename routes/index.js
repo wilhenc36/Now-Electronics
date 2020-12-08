165f1f0defcf284f198ed4bcb65c775e65371fa4
jaal99 <57971905+jaal99@users.noreply.github.com>
2020-12-08 03:22:47 -0600

    Implementacion de rutas para carrito, reducir y eliminar productos del carrito y vista de pago.

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const authController = require("../controllers/authController");
 const productoController = require("../controllers/productoController");
 const homeController = require("../controllers/homeController");
 const { check } = require("express-validator");
+const Carrito = require("../models/Carrito");
+const Producto = require("../models/Producto");
 
 // Configura y mantiene todos los endpoints en el servidor
 const router = express.Router();
@@ -95,19 +97,70 @@ module.exports = () => {
     productoController.crearProducto
   );
 
-  router.get(
-    "/producto/:url",
-    authController.verificarInicioSesion,
-    productoController.verProducto
-  );
+  router.get("/producto/:url", authController.verificarInicioSesion, productoController.verProducto);
 
-  router.get(
-    "/carrito/:url",
-    authController.verificarInicioSesion,
-    productoController.agregarProductoCarrito
-  );
+  router.get("/carrito/:id", authController.verificarInicioSesion, function (req, res, next) {
+    var productoId = req.params.id;
+    var carrito = new Carrito(req.session.carrito ? req.session.carrito : {});
 
-  router.get("/carrito", (req, res, next) => res.render("carrito"));
+    Producto.findById(productoId, function (err, producto) {
+      if (err) {
+        return res.redirect("/");
+      }
+      carrito.add(producto, producto.id);
+      req.session.carrito = carrito;
+      console.log(req.session.carrito);
+      res.redirect("/");
+    });
+    req.flash("messages", [
+      {
+        message: "Producto agregado a tu carrito de compras",
+        alertType: "success",
+      },
+    ]);
 
+  });
+
+  router.get("/carrito", authController.verificarInicioSesion, function (req, res, next) {
+    if (!req.session.carrito) {
+      return res.render("carrito", { producto: null });
+    }
+    var carrito = new Carrito(req.session.carrito);
+    res.render("carrito", { producto: carrito.generarArray(), precioTotal: carrito.precioTotal });
+  });
+
+  router.get('/eliminar/:id', (req, res, next) => {
+    var productoId = req.params.id;
+    var carrito = new Carrito(req.session.carrito ? req.session.carrito : { items: {} });
+    carrito.eliminarItems(productoId);
+    req.session.carrito = carrito;
+    res.redirect('/carrito');
+
+  });
+
+  router.get('/reducir/:id', (req, res, next) => {
+    var productoId = req.params.id;
+    var carrito = new Carrito(req.session.carrito ? req.session.carrito : { items: {} });
+    carrito.reducirItem(productoId);
+    req.session.carrito = carrito;
+    res.redirect('/carrito');
+
+  });
+
+  router.get("/pago", function(req, res, next) {
+    //var carrito = new Carrito(req.session.carrito);
+    
+    
+      req.session.carrito = null;
+      return res.render("pago")
+  });
+
+  /*
+    router.get(
+      "/carrito/:url",
+      authController.verificarInicioSesion,
+      productoController.agregarProductoCarrito
+    );
+  */
   return router;
 };
