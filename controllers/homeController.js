810fc80aca1b6fbee0bb767ff6eeb9458659ffe9
DavidAlejo07 <40010139+DavidAlejo07@users.noreply.github.com>
2020-12-09 22:52:33 -0600

    Implementacion de la funcion de roles de usuario y administrador.

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -5,5 +5,28 @@ exports.mostrarProductos = async (req, res, next) => {
   // Obtener todos los productos disponibles
   const productos = await Producto.find().lean();
 
-  res.render("buscar", { productos });
+  //roles
+  var usuario = false;
+  var admin = false;
+  var miron = false;
+  var rol, nombre; 
+  if(req.isAuthenticated()) {
+    rol = req.user.rol;
+    nombre = req.user.nombre;
+    if(rol == "usuario") {
+      usuario = true;
+    }
+  }
+  if(req.isAuthenticated()) {
+    rol = req.user.rol;
+    nombre = req.user.nombre;
+    if(rol == "admin") {
+      admin = true;
+    }
+  }
+  if(req.isAuthenticated() != true) {
+    miron = true;
+  }
+
+  res.render("buscar", { productos, usuario, admin, miron, nombre });
 };
