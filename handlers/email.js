9f0ef3e9e864af1a2cbc32a8c2aa66d240cc7cb1
Wilhen Cruz <wilhencruz36@yahoo.com>
2020-12-15 17:34:30 -0600

    Configuracion del email

--- handlers/email.js
+++ handlers/email.js
@@ -5,25 +5,14 @@ const hbs = require("nodemailer-express-handlebars");
 const util = require("util");
 
 var transport = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.etherael.email",
+  post: 587,
   secure: false,
   auth: {
     user: process.env.GMAIL,
     pass: process.env.GMAILPASS,
   },
 });
-
-/*
-// Configurar la capa de transporte del correo
-const transport = nodemailer.createTransport({
-    host: emailConfig.host,
-    port: emailConfig.port,
-    secure: false,
-    auth: {
-      user: emailConfig.user,
-      pass: emailConfig.pass,
-    },
-  });*/
 
 // Template para el envío del correo
 transport.use(
@@ -52,31 +41,6 @@ exports.enviarCorreo = async (opciones) => {
       nombre: opciones.nombre,
     },
   };
-
-  /*
-transport.sendMail(opcionesCorreo, function(err, info){
-  if(err){
-    console.log(err);
-  }
-  else{
-    console.log('Email Sent:' + info.response);
-  }
-});*/
-
-  /*
-// Encabezado del correo electrónico
-exports.enviarCorreo = async (opciones) => {
-  console.log(opciones);
-    const opcionesCorreo = {
-      from: " NowElectronics <nowelectronics001@gmail.com>",
-      to: opciones.to,
-      subject: opciones.subject,
-      template: opciones.template,
-      context: {
-        resetUrl: opciones.resetUrl,
-        //nombre: opciones.nombre,
-      },
-};*/
 
   // Enviar el correo mediante una promesa
   const sendMail = util.promisify(transport.sendMail, transport);
